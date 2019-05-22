7e10c30d9f1b103f505b0807caef0e907c43ba06
WillieApfel <williemr17@gmail.com>
2019-05-22 01:42:31 -0400

    Update

--- db_mercado.sql
+++ db_mercado.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-05-2019 a las 23:42:47
+-- Tiempo de generación: 22-05-2019 a las 07:41:49
 -- Versión del servidor: 10.1.39-MariaDB
 -- Versión de PHP: 7.3.5
 
@@ -78,7 +78,41 @@ CREATE TABLE `persona` (
 INSERT INTO `persona` (`id_persona`, `nombres`, `apellidos`, `correo`, `nombre_usuario`, `clave`) VALUES
 (27171641, 'Daniela ', 'Gomez', 'danielagomez007@hotmail.com', 'danielavgj', 'gomez998'),
 (27488851, 'Carlos Alberto', 'Zapata Cardozo', 'kabetsinespacios@gmail.com', 'kabet', 'salitre23'),
-(27877535, 'Willie Antonio', 'Manzano Rodríguez', 'williemr17@gmail.com', 'WillieApfel', '12345678');
+(27877535, 'Willie Antonio', 'Manzano Rodriguez', 'williemr17@gmail.com', 'WillieApfel', '12345678');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id_producto` int(20) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `tipo` varchar(20) NOT NULL,
+  `imagen` varchar(100) NOT NULL,
+  `precio` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`id_producto`, `nombre`, `tipo`, `imagen`, `precio`) VALUES
+(1, 'Aceite', 'Alimento', 'multimedia/img_productos/Alimentos/aceite.jpg', 10000),
+(2, 'Adobo', 'Alimento', 'multimedia/img_productos/Alimentos/adobo.jpg', 5000),
+(3, 'Arroz', 'Alimento', 'multimedia/img_productos/Alimentos/arroz.jpg', 10000),
+(4, 'Avena', 'Alimento', 'multimedia/img_productos/Alimentos/avena.jpg', 10000),
+(5, 'Cafe', 'Alimento', 'multimedia/img_productos/Alimentos/cafe.jpg', 5000),
+(6, 'Chicha', 'Alimento', 'multimedia/img_productos/Alimentos/chicha.jpg', 5000),
+(7, 'Nucita', 'Alimento', 'multimedia/img_productos/Alimentos/nucita.jpg', 10000),
+(8, 'Pepitona', 'Alimento', 'multimedia/img_productos/Alimentos/pepitona.jpg', 5000),
+(9, 'Jamon', 'Charcuteria', 'multimedia/img_productos/Charcuteria/jamon.png', 10000),
+(10, 'Jamon Serrano', 'Charcuteria', 'multimedia/img_productos/Charcuteria/jamon_serrano.png', 10000),
+(11, 'Mortadela', 'Charcuteria', 'multimedia/img_productos/Charcuteria/mortadela.png', 5000),
+(12, 'Queso Amarillo', 'Charcuteria', 'multimedia/img_productos/Charcuteria/queso_amarillo.png', 20000),
+(13, 'Queso Blanco', 'Charcuteria', 'multimedia/img_productos/Charcuteria/queso_blanco.png', 10000),
+(14, 'Salchichas', 'Charcuteria', 'multimedia/img_productos/Charcuteria/salchichas.png', 10000);
 
 --
 -- Índices para tablas volcadas
@@ -104,6 +138,12 @@ ALTER TABLE `persona`
   ADD UNIQUE KEY `nombre_usuario` (`nombre_usuario`);
 
 --
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id_producto`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -118,6 +158,12 @@ ALTER TABLE `contacto`
 --
 ALTER TABLE `persona`
   MODIFY `id_persona` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27877561;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id_producto` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
